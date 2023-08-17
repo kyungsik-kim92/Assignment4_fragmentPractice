d793d96433566b22efe4c7c0c1f58558e9acbae3
Kim Kyungsik <ksiki1@naver.com>
2023-08-17 16:30:11 +0900

    바텀 네비게이션 완성

--- app/src/main/java/com/example/assignment4/MainActivity.kt
+++ app/src/main/java/com/example/assignment4/MainActivity.kt
@@ -2,10 +2,61 @@ package com.example.assignment4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.assignment4.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding =ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        setupBottomNavigationView()
+
+        if (savedInstanceState == null) {
+            binding.bottomNavigationView.selectedItemId = R.id.fragment1
+        }
+
+
     }
+
+
+
+    private fun setupBottomNavigationView() {
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.fragment1 -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frameLayout, Fragment1())
+                        .commit()
+                    true
+                }
+
+                R.id.fragment2 -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frameLayout, Fragment2())
+                        .commit()
+                    true
+                }
+
+                R.id.fragment3 -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frameLayout, Fragment3())
+                        .commit()
+                    true
+                }
+                R.id.fragment4 -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frameLayout, Fragment4())
+                        .commit()
+                    true
+                }
+
+                else -> false
+            }
+        }
+    }
+
 }
